b88993aca0a2065cc5320a778f39979e4cf4fbb7
cgiacomi <cgi@hotboard.io>
2016-05-17 17:05:53 +0200

    File can be encrypted

--- hotbackup/__init__.py
+++ hotbackup/__init__.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import os
 import click
 import logging
+import datetime
+import boto3
 
-from hotbackup.utility import save_config
+from hotbackup.utility import save_config, load_config, write_encrypted
 
 
 __version__ = '0.0.1'
@@ -27,8 +30,39 @@ def cli(debug):
 
 
 @cli.command()
+@click.argument('filepath')
+@click.option('--password', type=str, help='Optional password used for encrypting the file.')
+def backup(filepath, password):
+  """Backup a file to Amazon AWS S3.
+
+  filepath: The full path to the file, including file name.
+
+  """
+  log.info('Initiating file backup.')
+  filename = os.path.basename(filepath)
+  config = load_config()
+  client = boto3.client('s3', region_name=config['aws_region_name'],
+                              aws_access_key_id=config['aws_access_key'],
+                              aws_secret_access_key=config['aws_secret_key'])
+
+  encrypted = False
+  now = datetime.datetime.utcnow()
+  stored_filename = '{0}.{1}'.format(filename, now.strftime('%Y%m%d%H%M%S'))
+
+  if password:
+    log.info('Encrypting file...')
+    stored_filename = '{0}.enc'.format(stored_filename)
+    filepath = write_encrypted(password, stored_filename, filepath)
+    encrypted = True
+
+  client.upload_file(filepath, 'swipster-backup', stored_filename)
+  log.info('File backup completed.')
+
+
+@cli.command()
 def configure():
   """Amazon AWS Configuration."""
+  log.info('Configuring application.')
   try:
     config = {}
     config['aws_access_key'] = click.prompt('AWS Access Key', type=str)
@@ -38,3 +72,6 @@ def configure():
     save_config(config)
   except KeyboardInterrupt:
     log.error('\nOperation cancelled by user.')
+
+  log.info('Configuring file saved.')
+
--- hotbackup/utility.py
+++ hotbackup/utility.py
@@ -4,8 +4,10 @@ import os
 import logging
 import yaml
 
+from simplecrypt import encrypt, decrypt
 
-__all__ = ['load_config', 'save_config']
+
+__all__ = ['load_config', 'save_config', 'read_encrypted', 'write_encrypted']
 
 
 log = logging.getLogger(__name__)
@@ -34,3 +36,44 @@ def save_config(config):
   """
   yaml.dump(config, open(CONFIG_FILE, 'w'), default_flow_style=False)
   log.debug('Config written in {0}'.format(CONFIG_FILE))
+
+
+def read_encrypted(password, filename, string=True):
+  """Decrypts a file
+
+  Args:
+    password (str): The password for the decryption.
+    filename (str): The output name of the clear text file.
+    string (bool): UTF-8 data or no, used for decoding.
+
+  Returns:
+    str: The descrypted data.
+
+  """
+  log.debug('Decrypting file.')
+  with open(filename, 'rb') as input:
+    ciphertext = input.read()
+    plaintext = decrypt(password, ciphertext)
+    if string:
+      return plaintext.decode('utf8')
+    else:
+      return plaintext
+
+
+def write_encrypted(password, filename, data):
+  """Encrypts a file
+
+  Args:
+    password (str): The password for the encryption.
+    filename (str): The output name of the encrypted file.
+    data (str): The path to the data to be encrypted.
+
+  Returns:
+    str: The name and path of the encrypted file.
+
+  """
+  with open(filename, 'wb') as output:
+    ciphertext = encrypt(password, data)
+    output.write(ciphertext)
+    log.debug('Encrypted file {0}'.format(output.name))
+    return output.name
